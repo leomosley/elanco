1fa1b3712405288f6989cdb86974ff07833fad4a
leomosley <leo.mosley2@gmail.com>
2024-11-25 00:06:51 +0000

    fix: move and implement search

--- components/home/countries.tsx
+++ components/home/countries.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { CountryPopulation } from '@/lib/utils';
 import Link from 'next/link';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import { Search } from './search';
 
 interface GroupedCountries {
   [key: string]: CountryPopulation[];
@@ -12,6 +16,8 @@ export default function Countries({
 }: {
   countries: CountryPopulation[]
 }) {
+  const [filter, setFilter] = useState('');
+
   const groupedCountries = countries.reduce((group, item) => {
     const firstLetter = item.country[0].toUpperCase();
     if (!group[firstLetter]) {
@@ -21,33 +27,55 @@ export default function Countries({
     return group;
   }, {} as GroupedCountries);
 
-  const letters = Object.keys(groupedCountries).sort();
+  const filteredGroupedCountries = () => {
+    if (!filter) return groupedCountries;
+
+    const filtered: Record<string, CountryPopulation[]> = {}
+    Object.entries(groupedCountries).forEach(([letter, countries]) => {
+      const filteredCountries = countries.filter(country =>
+        country.country.toLowerCase().includes(filter.toLowerCase())
+      )
+      if (filteredCountries.length > 0) {
+        filtered[letter] = filteredCountries;
+      }
+    })
+    return filtered;
+  }
+  const filtered = filteredGroupedCountries();
+
+  const letters = Object.keys(filtered).sort();
 
   return (
-    <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 justify-between">
-      {letters.map((letter) => (
-        <div key={letter} className="break-inside-avoid mb-4">
-          <Label className="text-lg font-medium">{letter}</Label>
-          <div className="flex flex-col gap-1.5">
-            {groupedCountries[letter].map((item) => (
-              <Link
-                key={item.code}
-                className="group max-w-fit"
-                href={item.country}
-                prefetch
-              >
-                <Button
-                  className="px-4 rounded-full"
-                  variant="outline"
-                  size="sm"
+    <div>
+      <div className="flex flex-col w-fit mb-2">
+        <h1 className="font-semibold text-lg">Search for a country</h1>
+        <Search value={filter} onChange={setFilter} />
+      </div>
+      <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 justify-between">
+        {letters.map((letter) => (
+          <div key={letter} className="break-inside-avoid mb-4">
+            <Label className="text-lg font-medium">{letter}</Label>
+            <div className="flex flex-col gap-1.5">
+              {filtered[letter].map((item) => (
+                <Link
+                  key={item.code}
+                  className="group max-w-fit"
+                  href={item.country}
+                  prefetch
                 >
-                  {item.country}
-                </Button>
-              </Link>
-            ))}
+                  <Button
+                    className="px-4 rounded-full"
+                    variant="outline"
+                    size="sm"
+                  >
+                    {item.country}
+                  </Button>
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
--- components/home/search.tsx
+++ components/home/search.tsx
@@ -1,12 +1,22 @@
 import { SearchIcon } from "lucide-react"
+import { SetStateAction } from "react";
 
-export default function Search() {
+export function Search({
+  value,
+  onChange
+}: {
+  value: string | undefined;
+  onChange: React.Dispatch<SetStateAction<string>>
+}) {
   return (
     <div className="flex items-center gap-2 p-1 rounded-lg bg-muted border shadow-sm">
       <SearchIcon className="w-4 h-4 text-muted-foreground" />
       <input
         placeholder="Search"
+        type="text"
         className="pr-12 outline-none bg-transparent text-sm"
+        onChange={(e) => onChange(e.currentTarget.value)}
+        value={value}
       />
     </div>
   );
